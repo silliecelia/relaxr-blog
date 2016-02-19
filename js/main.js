e4dd3223450ddfa5c737545cc02fdebc786afb87
Arissa "Reck" Brown <recked@users.noreply.github.com>
2016-02-19 17:05:34 -0500

    review pt 2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,13 @@
-                var signUpNow = $('.sign-up-now-btn');
-                signUpNow.click(function() {
-                    alert("We are not currently taking sign ups");
-                    signUpNow.html('No Sign Ups!')
-                });
+var signUpNow = $('.sign-up-now-btn');
+signUpNow.click(function() {
+    alert("We are not currently taking sign ups");
+    signUpNow.html('No Sign Ups!');
+});
 
-                $(".more-text").hide();
-                
-                var readMore = $('.read-more');
-                readMore.click(function(){
-                $("p.more-text").slideToggle();
-                });
+$(".more-text").hide();
 
-
-    // var txt = $('.sign-up-now-btn');
-    // $(txt).on('click', function(){
-    //     alert('ad blocker');
-    //     txt.html('yeah biddy');    
-    // });            
-
-
-    // alert('working');            
+var readMore = $('.read-more');
+readMore.click(function(){
+  console.log('clicked');
+  $(".more-text").show();
+});
