1fd7a16ef044b309774b18ea269c376cc22a8457
silliecelia <chungsyc@gmail.com>
2016-02-22 14:43:17 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -6,29 +6,15 @@
 
                 $(".more-text").hide();
 
-                var show = false;
-                if (show) {
-                    hide();
-                    show = false;
-                } else {
-                    show();
-                    show = true;
-                }
-
                 var readMore = $('.read-more');
+                
                 readMore.click(function(){
-                if $(".more-text").show();
-                } else {
-
+                    if (show) {
+                        $(".more-text").hide();
+                    } else {
+                        $(".more-text").show();
                 }
-
-                });
-
-                var readLess = $('.read-more');
-                readLess.click(function() {
-                readLess.html('Read Less');
-                $(".more-text").hide();
-                });
+            });
 
 
     // var txt = $('.sign-up-now-btn');
