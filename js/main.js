bd02a1d1f8710bb9a0a2d6e2a97238b5b0e5e3a1
silliecelia <chungsyc@gmail.com>
2016-02-22 19:59:25 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
-                var signUpNow = $('.sign-up-now-btn');
-                signUpNow.click(function() {
-                    alert("We are not currently taking sign ups");
-                    signUpNow.html('No Sign Ups!')
-                });
+var signUpNow = $('.sign-up-now-btn');
+signUpNow.click(function() {
+    alert("We are not currently taking sign ups");
+    signUpNow.html('No Sign Ups!');
+});
 
-                $(".more-text").hide();
-                var show = false;
+$(".more-text").hide();
 
-                var readMore = $('.read-more');
-                
-                readMore.click(function(){
-                    if (show) {
-                        $(".more-text").hide();
-                    } else {
-                        $(".more-text").show();
-                }
-            });
+var show = false;
+var readMore = $('.read-more');
+
+readMore.click(function(){
+    if (show) {
+        show = false;
+        readMore.text('Read More >');
+        $(".more-text").slideUp();
+    } else {
+        show = true;
+        $(".more-text").slideDown();
+        readMore.text('Read Less >');
+}
+});
+
+$(".more-text-2").hide();
+
+var show = false;
+var learnMore = $('.learn-more');
+
+learnMore.click(function(){
+    if (show) {
+        show = false;
+        $(".more-text-2").slideUp(200);
+    } else {
+        show = true;
+        $(".more-text-2").slideDown(200);
+}
+});
 
 
     // var txt = $('.sign-up-now-btn');
